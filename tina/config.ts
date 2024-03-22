6d56175cb27dc0bddafacf06ff1dda7c1581c998
ducwp <ducwebvn@gmail.com>
2024-03-22 13:31:51 +0700

    Tina

---
+++ tina/config.ts
@@ -0,0 +1,123 @@
+import { defineConfig } from "tinacms";
+
+import { authorFields } from "./templates";
+import { home_heroFields } from "./templates";
+import { homeFields } from "./templates";
+import { postFields } from "./templates";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  client: { skip: true },
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        format: "md",
+        label: "Home",
+        name: "home",
+        path: "content",
+        frontmatterFormat: "yaml",
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+        },
+        match: {
+          include: "_index",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        format: "md",
+        label: "Posts",
+        name: "posts",
+        path: "content/post",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...postFields(),
+        ],
+      },
+      {
+        format: "md",
+        label: "Authors",
+        name: "authors",
+        path: "content/authors",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/_index",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+          ...authorFields(),
+        ],
+      },
+      {
+        format: "yaml",
+        label: "Config",
+        name: "config",
+        path: "config/_default",
+        frontmatterFormat: "yaml",
+        match: {
+          include: "**/*",
+        },
+        fields: [
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body of Document",
+            description: "This is the markdown body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
